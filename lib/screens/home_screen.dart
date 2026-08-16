bbedf677ad4b10bfff6dd0da28fc812978a4d355
mohannad-090 <mohanadosama176@gmail.com>
2026-08-16 15:52:54 +0300

    home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:menuhati/widgets/coffe_card_gridveiw.dart';
+import 'package:menuhati/widgets/custom_app_bar.dart';
+import 'package:menuhati/widgets/text_tile.dart';
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
@@ -6,7 +9,57 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     
+      body: SafeArea(
+        child: Column(
+          children: [
+            AppHeader(
+              showCartIcon: true,
+              title: 'Menuhati',
+            ),
+            
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    TextTile(
+                      title: 'Find your coffee ☕',
+                      subTitle: 'Discover artisanal roasters and local spots.',
+                    ),
+                    
+                    const SizedBox(height: 48),
+                    
+                    Expanded(
+                      child: CoffeeCardGridView(
+                        crossAxisCount: 2,
+                        crossAxisSpacing: 16.0,
+                        mainAxisSpacing: 16.0,
+                        padding: const EdgeInsets.only(
+                          top: 0,
+                          bottom: 16.0,
+                          left: 0,
+                          right: 0,
+                        ),
+                        // onItemTap: (item) {
+                        //   Navigator.push(
+                        //     context,
+                        //     MaterialPageRoute(
+                        //       builder: (context) => CoffeeDetailsPage(
+                        //         coffee: item,
+                        //       ),
+                        //     ),
+                        //   );
+                        // },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
